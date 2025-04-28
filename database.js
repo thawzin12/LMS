8f8dde5e36eb5f259ba1d85d81422aebb594f964
ThawZinHtet <thawhtet077@gmail.com>
2025-04-28 10:03:43 +0630

    the end of the lms

--- database.js
+++ database.js
@@ -1,25 +1,23 @@
-
 // database section
-const mysql = require('mysql2');
-
+const mysql = require("mysql2");
 
 // Replace 'your_username', 'your_password', 'your_database', and 'your_host' with your MySQL credentials
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'thawzinhtet@#',
-    database: 'test',
-    connectionLimit: 200
+  host: "localhost",
+  user: "root",
+  password: "thawzinhtet@#",
+  database: "test",
+  connectionLimit: 200,
 });
 
 //Connect to the database
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        return;
-    }
-    console.log('Connected to MySQL database');
-
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
 // Perform database operations here
 
 //insert  into data
@@ -43,4 +41,3 @@ connection.connect((err) => {
 //     }
 //     console.log('Query results:', results);
 //     console.log('Query results:', fields);
-
